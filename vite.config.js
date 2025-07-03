5ddf94fde7ba4a6043198864adcf7837fe693b46
Matt-PY-GH <mateldraka@gmail.com>
2025-07-03 00:57:56 -0300

    TODO FIXEADO

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,4 @@ export default defineConfig({
   }
 })
 
-//Esta es la configuración de vite que tuve que hacer para que sean servidos correctamente ambos html, de lo contrario tenía errores en producción, los cuales pude ver haciendo uso de npm run preview. Simplemente agrega la ruta de detalles.html para que no rompa ni lance errores.
+//Esta es la configuración de vite que tuve que hacer para que sean servidos correctamente ambos html, de lo contrario tenía errores en producción, los cuales pude ver haciendo uso de npm run preview. Simplemente agrega las rutas para que no rompa ni lance errores.
